f12fef11fe873f24e0aebef6d171c96a61e4b8d3
ns34sh <103830896+ns34sh@users.noreply.github.com>
2023-05-13 03:42:21 +0530

    Files uploaded
    First version of files uploaded

---
+++ Form1.Designer.vb
@@ -0,0 +1,198 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        Button2 = New Button()
+        Label_time = New Label()
+        Label2 = New Label()
+        ProgressBar1 = New ProgressBar()
+        Label3 = New Label()
+        Label4 = New Label()
+        StartLC = New TextBox()
+        EndLC = New TextBox()
+        Label_max_str = New Label()
+        LC_label = New Label()
+        Label_max_str_value = New Label()
+        Label_max_str_LC = New Label()
+        Label_time_remaining = New Label()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(116, 106)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(248, 56)
+        Button1.TabIndex = 0
+        Button1.Text = "Calculate Strain Energy"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(191, 349)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(93, 39)
+        Button2.TabIndex = 2
+        Button2.Text = "Cancel"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Label_time
+        ' 
+        Label_time.AutoSize = True
+        Label_time.Location = New Point(79, 185)
+        Label_time.Name = "Label_time"
+        Label_time.Size = New Size(164, 25)
+        Label_time.TabIndex = 5
+        Label_time.Text = "Estimated time left:"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(243, 122)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(0, 25)
+        Label2.TabIndex = 6
+        ' 
+        ' ProgressBar1
+        ' 
+        ProgressBar1.Location = New Point(161, 226)
+        ProgressBar1.Name = "ProgressBar1"
+        ProgressBar1.Size = New Size(150, 34)
+        ProgressBar1.TabIndex = 7
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(66, 23)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(200, 25)
+        Label3.TabIndex = 8
+        Label3.Text = "Start Load Combination"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(66, 65)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(194, 25)
+        Label4.TabIndex = 9
+        Label4.Text = "End Load Combination"
+        ' 
+        ' StartLC
+        ' 
+        StartLC.Location = New Point(272, 20)
+        StartLC.Name = "StartLC"
+        StartLC.Size = New Size(150, 31)
+        StartLC.TabIndex = 10
+        ' 
+        ' EndLC
+        ' 
+        EndLC.Location = New Point(272, 65)
+        EndLC.Name = "EndLC"
+        EndLC.Size = New Size(150, 31)
+        EndLC.TabIndex = 11
+        ' 
+        ' Label_max_str
+        ' 
+        Label_max_str.AutoSize = True
+        Label_max_str.Location = New Point(79, 278)
+        Label_max_str.Name = "Label_max_str"
+        Label_max_str.Size = New Size(202, 25)
+        Label_max_str.TabIndex = 12
+        Label_max_str.Text = "Maximum strain energy:"
+        ' 
+        ' LC_label
+        ' 
+        LC_label.AutoSize = True
+        LC_label.Location = New Point(79, 309)
+        LC_label.Name = "LC_label"
+        LC_label.Size = New Size(214, 25)
+        LC_label.TabIndex = 13
+        LC_label.Text = "Corresponding load case:"
+        ' 
+        ' Label_max_str_value
+        ' 
+        Label_max_str_value.AutoSize = True
+        Label_max_str_value.Location = New Point(272, 278)
+        Label_max_str_value.Name = "Label_max_str_value"
+        Label_max_str_value.Size = New Size(22, 25)
+        Label_max_str_value.TabIndex = 14
+        Label_max_str_value.Text = "0"
+        ' 
+        ' Label_max_str_LC
+        ' 
+        Label_max_str_LC.AutoSize = True
+        Label_max_str_LC.Location = New Point(289, 309)
+        Label_max_str_LC.Name = "Label_max_str_LC"
+        Label_max_str_LC.Size = New Size(22, 25)
+        Label_max_str_LC.TabIndex = 15
+        Label_max_str_LC.Text = "0"
+        ' 
+        ' Label_time_remaining
+        ' 
+        Label_time_remaining.AutoSize = True
+        Label_time_remaining.Location = New Point(244, 185)
+        Label_time_remaining.Name = "Label_time_remaining"
+        Label_time_remaining.Size = New Size(22, 25)
+        Label_time_remaining.TabIndex = 16
+        Label_time_remaining.Text = "0"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(506, 415)
+        Controls.Add(Label_time_remaining)
+        Controls.Add(Label_max_str_LC)
+        Controls.Add(Label_max_str_value)
+        Controls.Add(LC_label)
+        Controls.Add(Label_max_str)
+        Controls.Add(EndLC)
+        Controls.Add(StartLC)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(ProgressBar1)
+        Controls.Add(Label2)
+        Controls.Add(Label_time)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "Calculate Strain Energy"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label_time As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents StartLC As TextBox
+    Friend WithEvents EndLC As TextBox
+    Friend WithEvents Label_max_str As Label
+    Friend WithEvents LC_label As Label
+    Friend WithEvents Label_max_str_value As Label
+    Friend WithEvents Label_max_str_LC As Label
+    Friend WithEvents Label_time_remaining As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,299 @@
+﻿Imports System.Windows.Forms
+Imports System.ComponentModel
+Imports System.Threading
+Imports System.Reflection.PortableExecutable
+Imports System.Configuration
+''Calculate strain energy subroutine contains the actual code for calculating strain energy
+Public Class Form1
+    Private _progressBar As ProgressBar
+    Private _worker As BackgroundWorker
+
+    Sub New()
+        ' This call is required by the designer.
+        InitializeComponent()
+        Initialize()
+        BindComponent()
+    End Sub
+
+    Private Sub Initialize()
+        _progressBar = ProgressBar1
+        '_progressBar.Dock = DockStyle.Fill
+
+        _worker = New BackgroundWorker()
+        _worker.WorkerReportsProgress = True
+        _worker.WorkerSupportsCancellation = True
+
+        'Me.Controls.Add(_progressBar)
+    End Sub
+
+    Private Sub BindComponent()
+        AddHandler _worker.ProgressChanged, AddressOf _worker_ProgressChanged
+        AddHandler _worker.RunWorkerCompleted, AddressOf _worker_RunWorkerCompleted
+        AddHandler _worker.DoWork, AddressOf _worker_DoWork
+        AddHandler Me.Load, AddressOf Form1_Load
+    End Sub
+
+    Private Sub Form1_Load()
+
+    End Sub
+
+    Private Sub _worker_ProgressChanged(ByVal o As Object, ByVal e As ProgressChangedEventArgs)
+
+        'Update progress bar in GUI form
+        _progressBar.Increment(e.ProgressPercentage)
+
+    End Sub
+
+    Private Sub _worker_RunWorkerCompleted(ByVal o As Object, ByVal e As RunWorkerCompletedEventArgs)
+
+        If (e.Cancelled) Then
+            'If user cancels the process then display a message
+            MsgBox("Process was interrupted by the user.")
+        ElseIf e.Error IsNot Nothing Then
+            'Display an error message if there is an error
+            MsgBox(e.Error.Message)
+        Else
+            'Display an final output for maximum strain energy and the corresponding load case in GUI.
+            Label_max_str_value.Text = e.Result(0)
+            Label_max_str_LC.Text = e.Result(1)
+            MsgBox("Done.")
+        End If
+    End Sub
+
+    Private Sub _worker_DoWork(ByVal o As Object, ByVal e As DoWorkEventArgs)
+        Dim worker = DirectCast(o, BackgroundWorker)
+        'Run code to calculate strain energy
+        calculate_strain_energy(worker, e)
+
+    End Sub
+
+    Private Sub SetProgressMaximum(ByVal max As Integer)
+        If _progressBar.InvokeRequired Then
+            _progressBar.Invoke(Sub() SetProgressMaximum(max))
+        Else
+            _progressBar.Maximum = max
+        End If
+    End Sub
+
+    Public Sub calculate_strain_energy(worker, e)
+
+        Dim objOpenSTAAD As Object
+        'Create COM object to access STAADpro software API commands
+        objOpenSTAAD = GetObject(, "StaadPro.OpenSTAAD")
+
+        'Check if the COM object was created
+        If objOpenSTAAD Is Nothing Then
+            MsgBox("Please ensure STAAD file is open.")
+            End
+        End If
+
+        'Check if the FEM analysis results are available 
+        If objOpenSTAAD.Output.AreResultsAvailable() = False Then
+            MsgBox("Please ensure results are available in STAAD.")
+            End
+        End If
+
+        Dim BaseUnit As Double
+        'Get Base Unit
+        '1=English
+        '2=Metric
+
+        'Get the base unit used in the FEM analysis
+        BaseUnit = objOpenSTAAD.GetBaseUnit
+
+        Dim strain_energy_unit As String
+
+        'Decide the output units to be displayed based on the base unit
+        If BaseUnit = 1 Then
+            strain_energy_unit = "kip-in"
+        Else
+            strain_energy_unit = "kN-m"
+        End If
+
+
+        Dim SelbeamsNo As Long
+
+        Dim Selbeams() As Long
+
+        'Get no. of beams selected by the user (in STAADpro software GUI)
+        SelbeamsNo = objOpenSTAAD.Geometry.GetNoOfSelectedBeams
+
+        'Prompt user to select beam
+        If SelbeamsNo = 0 Then
+            MsgBox("Please select members in STAAD.")
+            End
+        End If
+
+        'Reallocate array for storing selected beam numbers
+        ReDim Selbeams(SelbeamsNo - 1)
+        'Populate Selbeams array using API command
+        objOpenSTAAD.Geometry.GetSelectedBeams(Selbeams, 1)
+
+        'Declare beam property variables
+        Dim Width As Double
+        Dim Depth As Double
+        Dim Ax As Double
+        Dim Ay As Double
+        Dim Az As Double
+        Dim Ix As Double
+        Dim Iy As Double
+        Dim Iz As Double
+
+        Dim BeamLen As Double
+
+        'Declare array for storing forces in beam
+        Dim ForceArray(0 To 5) As Double
+
+        'Store start and end load cases entered by the user in the form
+        Dim start_load_case = CLng(e.argument(0))
+        Dim end_load_case = CLng(e.argument(1))
+
+        'Prompt user to enter valid load case values
+        If start_load_case = 0 Or end_load_case = 0 Then
+            MsgBox("Enter a valid integer value for start/end load cases.")
+        End If
+
+        'Declare variables to store strain energy contribution from different forces (i.e. axial, bending, torsion)
+        Dim strain_energy(end_load_case - start_load_case + 1) As Double
+        Dim strain_energy_mem As Double
+        Dim strain_energy_axial As Double
+        Dim strain_energy_my As Double
+        Dim strain_energy_mz As Double
+        Dim strain_energy_mx As Double
+
+        'Calculate total calculations to be done. This will be used to update the progress bar
+        'and to estimate the time remaining
+        Dim max_progress = SelbeamsNo * (end_load_case - start_load_case + 1)
+        SetProgressMaximum(max_progress)
+        Dim progress_val = 0
+
+        'Declare variable for showing progress to the user
+        Dim pctCompl As Double
+        Dim time_remaining As Double
+        Static start_time As DateTime
+        Static stop_time As DateTime
+        Dim elapsed_time As TimeSpan
+
+        'Declare variables to store beam material property
+        Dim dElasticity As Double, dPoisson As Double, dDensity As Double, dAlpha As Double, dDamp As Double
+
+        'Start timer (This will be used to estimate remaining time based on linear proportion)
+        'For example, if time taken to complete x percent work is y seconds then time required to do
+        'the remaining 100-x percent work is y / x * (100 - x) seconds.
+        start_time = Now
+
+        'Set maximum number of threads for parallel for loop
+        Dim par_opt As New ParallelOptions
+        par_opt.MaxDegreeOfParallelism = -1
+        'Use parallel for loop for outer loop that iterates over the user-specified load cases
+        Parallel.For(start_load_case, end_load_case, par_opt,
+                     Sub(j)
+                         'Inner loop iterates over all the beams selected by the user in STAADpro GUI
+                         For i = 0 To SelbeamsNo - 1
+
+                             'Get beam properties using API command
+                             objOpenSTAAD.Property.GetBeamProperty(Selbeams(i), Width, Depth, Ax, Ay, Az, Ix, Iy, Iz)
+                             'Get beam length using API command
+                             BeamLen = objOpenSTAAD.Geometry.GetBeamLength(Selbeams(i))
+
+                             'Get Elasticity using API command
+                             objOpenSTAAD.Property.GetBeamConstants(Selbeams(i), dElasticity, dPoisson, dDensity, dAlpha, dDamp)
+
+                             'Inner loops calculates stain energy at every 1/10th location of the beam length
+                             For k = 0 To BeamLen Step Math.Round(BeamLen / 10, 3)
+                                 'Get forces at various sections along the beam length
+                                 objOpenSTAAD.Output.GetIntermediateMemberForcesAtDistance(CLng(i), k, CLng(j), ForceArray)
+
+                                 'Calculate strain energy due to axial load in the beam
+                                 strain_energy_axial = ForceArray(0) ^ 2 * (BeamLen / 10) / (2 * Ax * dElasticity)
+
+                                 'Calculate strain energy due to minor axis bending in the beam
+                                 strain_energy_my = ForceArray(4) ^ 2 * (BeamLen / 10) / (2 * Iy * dElasticity)
+
+                                 'Calculate strain energy due to major axis bending in the beam
+                                 strain_energy_mz = ForceArray(5) ^ 2 * (BeamLen / 10) / (2 * Iz * dElasticity)
+
+                                 'Calcuate strain energy due to torsion in the beam
+                                 strain_energy_mx = ForceArray(3) ^ 2 * (BeamLen / 10) / (2 * Ix * dElasticity)
+
+                                 'Sum up the total strain energy due to each of the four components calculated above
+                                 strain_energy_mem = strain_energy_axial + strain_energy_my + strain_energy_mz + strain_energy_mx
+
+                                 'Store the strain energy calculated in an array for each load case
+                                 strain_energy(j) += strain_energy_mem
+                             Next k
+                             'Update progress
+                             progress_val = (i) * (end_load_case - start_load_case + 1) + (j - start_load_case + 1)
+                             'Calcuate percentage completion
+                             pctCompl = progress_val / max_progress * 100
+                             'Report progress to GUI
+                             worker.ReportProgress(pctCompl)
+
+                             'Stop timer
+                             stop_time = Now
+                             'Calculate elapsed time
+                             elapsed_time = stop_time.Subtract(start_time)
+                             'Start timer again
+                             start_time = Now
+                             'Estimate time remaining
+                             time_remaining = Math.Round(elapsed_time.TotalSeconds / pctCompl * (100 - pctCompl), 0)
+
+                             'Update label in GUI to indicate time remaining to the user 
+                             Label_time_remaining.Text = time_remaining.ToString & " sec"
+
+                             'If user has clicked the cancel button, abort the proess
+                             If _worker.CancellationPending Then
+                                 e.Cancel = True
+                                 _worker.ReportProgress(0)
+                             End If
+
+
+                         Next i
+                     End Sub)
+
+        'Declare variables to store the final output
+        Dim max_value As Double = 0
+        Dim max_LC As Int16 = 0
+
+        'Find maximum strain energy and the corresponding load case
+        For j = 0 To UBound(strain_energy)
+            If strain_energy(j) > max_value Or j = 0 Then
+                max_value = strain_energy(j)
+                max_LC = start_load_case + j
+            End If
+        Next
+
+        'Store results in an array and assign array to e.result
+        Dim result_arr(1) As String
+        result_arr(0) = max_value.ToString
+        result_arr(1) = max_LC.ToString
+        e.result = result_arr
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Check if background worker is already busy running the asynchronous operation
+        'Will avoid error if user clicks the button "calculate strain energy" multiple times while the program is still running
+        If Not _worker.IsBusy Then
+            Dim LC_arr(1) As String
+            'Store user-input in LC_arr variable
+            LC_arr(0) = StartLC.Text
+            LC_arr(1) = EndLC.Text
+            'Start execution asynchronously in the background
+            'Also pass LC_arr as argument
+            _worker.RunWorkerAsync(LC_arr)
+        End If
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        'Check if background worker is running
+        'Will cancel the process if the user clicks the cancel button
+        If _worker.IsBusy Then
+            'Cancel the asynchronous operation if still in progress
+            _worker.CancelAsync()
+        End If
+        End
+    End Sub
+
+End Class
